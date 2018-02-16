60b30dddfc5effc4000c3de2ff940a81e30dfc09
FontBenjamin <font.benjamin@yahoo.fr>
2018-02-16 09:29:28 +0100

    Added custom bar

--- app/src/main/java/com/example/benjamin/mobileproject_2018/MainActivity.java
+++ app/src/main/java/com/example/benjamin/mobileproject_2018/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.benjamin.mobileproject_2018;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        actionBar.setCustomView(R.layout.statusbar);
+
     }
 }
