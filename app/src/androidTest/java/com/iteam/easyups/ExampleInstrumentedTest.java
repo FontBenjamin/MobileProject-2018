5fd18c30ed5f9d9e90717d93409ff92d140f6cd7
Marianna-dl <marianna95220@gmail.com>
2018-02-23 10:31:56 +0100

    Add firebase dependancy

--- app/src/androidTest/java/com/iteam/easyups/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/iteam/easyups/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.benjamin.mobileproject_2018", appContext.getPackageName());
+        assertEquals("com.iteam.easyups", appContext.getPackageName());
     }
 }
