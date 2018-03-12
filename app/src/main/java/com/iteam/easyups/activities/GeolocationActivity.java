0dfd01a4cde19139b9ca5cf91e8706ed8d2c375b
LeaFoissac <lea.foissac@gmail.com>
2018-03-12 22:15:47 +0100

    Add points of interest

--- app/src/main/java/com/iteam/easyups/activities/GeolocationActivity.java
+++ app/src/main/java/com/iteam/easyups/activities/GeolocationActivity.java
@@ -5,18 +5,14 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -36,7 +32,6 @@ import com.google.firebase.database.ValueEventListener;
 import com.iteam.easyups.R;
 import com.iteam.easyups.adapter.PlaceSpinnerAdapter;
 import com.iteam.easyups.communication.DatabaseConnection;
-import com.iteam.easyups.model.Amphitheater;
 import com.iteam.easyups.model.Place;
 
 import java.util.ArrayList;
@@ -51,7 +46,7 @@ public class GeolocationActivity extends AppCompatActivity implements OnMapReady
     private Context context;
 
     private final List<Place> buildingList = new ArrayList<>();
-    private final List<Amphitheater> amphitheaterList = new ArrayList<>();
+    private final List<Place> amphitheaterList = new ArrayList<>();
     private final List<Place> poiList = new ArrayList<>();
 
     private GoogleMap map;
@@ -78,8 +73,8 @@ public class GeolocationActivity extends AppCompatActivity implements OnMapReady
         removePoiButton = (Button) findViewById(R.id.remove_poi);
 
         buildingManagement();
+        amphitheaterManagement();
         poiManagement();
-        initializeAmphitheatersSpinner();
         removePoi();
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
@@ -127,6 +122,35 @@ public class GeolocationActivity extends AppCompatActivity implements OnMapReady
 
         PlaceSpinnerAdapter dataAdapter = new PlaceSpinnerAdapter(context, R.layout.support_simple_spinner_dropdown_item, buildingList);
         buildingSpinner.setAdapter(dataAdapter);
+    }
+
+    public void amphitheaterManagement()
+    {
+        DatabaseReference ref = database.getReference("easyups/amphitheater");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                GenericTypeIndicator<List<Place>> genericTypeIndicator = new GenericTypeIndicator<List<Place>>(){};
+                amphitheaterList.addAll(dataSnapshot.getValue(genericTypeIndicator));
+                addAmphitheatersOnSpinner();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
+        });
+    }
+
+
+    public void addAmphitheatersOnSpinner()
+    {
+        Place place = new Place();
+        place.setName("Amphis");
+        amphitheaterList.add(0, place);
+
+        PlaceSpinnerAdapter dataAdapter = new PlaceSpinnerAdapter(context, R.layout.support_simple_spinner_dropdown_item, amphitheaterList);
+        amphitheaterSpinner.setAdapter(dataAdapter);
     }
 
     public void poiManagement()
@@ -196,18 +220,6 @@ public class GeolocationActivity extends AppCompatActivity implements OnMapReady
         poiSpinner.setAdapter(dataAdapter);
     }
 
-    public void initializeAmphitheatersSpinner()
-    {
-        List<String> amphitheatersNames = new ArrayList<>();
-        amphitheatersNames.add("Amphis");
-
-        ArrayAdapter<String> dataAdapter = getArrayAdapter(amphitheatersNames);
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        amphitheaterSpinner.setEnabled(false);
-        amphitheaterSpinner.setClickable(false);
-        amphitheaterSpinner.setAdapter(dataAdapter);
-    }
-
     public void createPoiOnMap()
     {
         map.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
@@ -251,49 +263,6 @@ public class GeolocationActivity extends AppCompatActivity implements OnMapReady
         }
     }
 
-    public void loadAmphitheatersOnSpinner(final String buildingName)
-    {
-        if (!amphitheaterList.isEmpty())
-        {
-            addAmphitheatersOnSpinner(buildingName);
-        }
-        else
-        {
-            DatabaseReference ref = database.getReference("easyups/amphitheater");
-            ref.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    GenericTypeIndicator<List<Amphitheater>> genericTypeIndicator = new GenericTypeIndicator<List<Amphitheater>>(){};
-                    amphitheaterList.addAll(dataSnapshot.getValue(genericTypeIndicator));
-                    addAmphitheatersOnSpinner(buildingName);
-                }
-
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-                    System.out.println("The read failed: " + databaseError.getCode());
-                }
-            });
-        }
-    }
-
-    public void addAmphitheatersOnSpinner(String buildingName)
-    {
-        amphitheaterSpinner.setEnabled(true);
-        amphitheaterSpinner.setClickable(true);
-
-        List<String> amphitheaterNames = new ArrayList<>();
-        amphitheaterNames.add("Amphis");
-        for (Amphitheater amphitheater : amphitheaterList)
-        {
-            if (amphitheater.getBuilding().equals(buildingName))
-            {
-                amphitheaterNames.add(amphitheater.getName());
-            }
-        }
-        ArrayAdapter<String> dataAdapter = getArrayAdapter(amphitheaterNames);
-        amphitheaterSpinner.setAdapter(dataAdapter);
-    }
-
     public void removePoi()
     {
         removePoiButton.setOnClickListener(new View.OnClickListener() {
@@ -318,7 +287,6 @@ public class GeolocationActivity extends AppCompatActivity implements OnMapReady
                 {
                     Place building = (Place) parent.getItemAtPosition(position);
                     if (building != null) {
-                        loadAmphitheatersOnSpinner(building.getName());
                         LatLng latLng = new LatLng(building.getLatitude(), building.getLongitude());
                         map.clear();
                         Marker currentMarker = map.addMarker(new MarkerOptions().position(latLng).title("Bâtiment " + building.getName()));
@@ -326,22 +294,26 @@ public class GeolocationActivity extends AppCompatActivity implements OnMapReady
                         map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 19));
 
                         poiSpinner.setSelection(0);
+                        amphitheaterSpinner.setSelection(0);
                         removePoiButton.setEnabled(false);
                     }
                 }
                 break;
             case R.id.amphitheaters:
-                String amphitheaterName = parent.getItemAtPosition(position).toString();
-                Amphitheater amphitheater = getAmphitheater(amphitheaterName);
-                if (amphitheater != null) {
-                    LatLng latLng = new LatLng(amphitheater.getLatitude(), amphitheater.getLongitude());
-                    map.clear();
-                    Marker currentMarker = map.addMarker(new MarkerOptions().position(latLng).title("Amphi " + amphitheater.getName()));
-                    currentMarker.showInfoWindow();
-                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 19));
+                if (position != 0)
+                {
+                    Place amphitheater = (Place) parent.getItemAtPosition(position);
+                    if (amphitheater != null) {
+                        LatLng latLng = new LatLng(amphitheater.getLatitude(), amphitheater.getLongitude());
+                        map.clear();
+                        Marker currentMarker = map.addMarker(new MarkerOptions().position(latLng).title("Amphi " + amphitheater.getName()));
+                        currentMarker.showInfoWindow();
+                        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 19));
 
-                    poiSpinner.setSelection(0);
-                    removePoiButton.setEnabled(false);
+                        buildingSpinner.setSelection(0);
+                        poiSpinner.setSelection(0);
+                        removePoiButton.setEnabled(false);
+                    }
                 }
                 break;
             case R.id.poi:
@@ -367,60 +339,6 @@ public class GeolocationActivity extends AppCompatActivity implements OnMapReady
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
-    }
-
-    public Place getBuilding(String name)
-    {
-        for (Place building : buildingList) {
-            if (building.getName().equals(name))
-            {
-                return building;
-            }
-        }
-        return null;
-    }
-
-    public Amphitheater getAmphitheater(String name)
-    {
-        for (Amphitheater amphitheater : amphitheaterList) {
-            if (amphitheater.getName().equals(name))
-            {
-                return amphitheater;
-            }
-        }
-        return null;
-    }
-
-    public ArrayAdapter<String> getArrayAdapter(List<String> names)
-    {
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, names){
-            @Override
-            public boolean isEnabled(int position){
-                if(position == 0)
-                {
-                    return false;
-                }
-                else
-                {
-                    return true;
-                }
-            }
-            @Override
-            public View getDropDownView(int position, View convertView, ViewGroup parent) {
-                View view = super.getDropDownView(position, convertView, parent);
-                TextView tv = (TextView) view;
-                if(position == 0){
-                    // Set the hint text color gray
-                    tv.setTextColor(Color.GRAY);
-                }
-                else {
-                    tv.setTextColor(Color.BLACK);
-                }
-                return view;
-            }
-        };
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        return dataAdapter;
     }
 
 }
